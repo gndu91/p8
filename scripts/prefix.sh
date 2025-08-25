1875316fe4c43eb1905e0d3ebcb0cadf2fbd0174
Nadir GHOUL <nadir@ghoul.fr>
2025-08-25 18:45:13 +0200

    Added an improved version of the p7 scripts

---
+++ scripts/prefix.sh
@@ -0,0 +1,20 @@
+# All the scripts will start the same way, by relocating and activating the environment
+
+# This will exit at the first failure
+# The most probable point of failure would be a non existent venv, to prevent messing with the global python env
+set -e
+
+test "${BASH_SOURCE[0]}" == "$0" && {
+  echo WARNING: This script needs to be sourced, not run > /dev/stderr
+  exit 1
+}
+
+# TODO: Auto create environment
+
+echo "Navigating to project's root..."
+cd "$(dirname "$(dirname "$(realpath "${BASH_SOURCE[0]}")")")"
+echo "Navigating to project's root...done"
+
+echo "Activating environment..."
+source "$(pwd).13.env/bin/activate"
+echo "Activating environment...done"
---
+++ scripts/update.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+# This will exit at the first failure
+# The most probable point of failure would be a non existent venv, to prevent messing with the global python env
+set -e
+
+echo "Navigating to project's root..."
+cd "$(dirname "$(dirname "$(realpath "${BASH_SOURCE[0]}")")")"
+echo "Navigating to project's root...done"
+
+echo "Activating environment..."
+source "$(pwd).13.env/bin/activate"
+echo "Activating environment...done"
+
+echo "Updating..."
+pip install -U pip
+pip install -Ur requirements.txt
+echo "Updating...done"
+
+echo "Freezing..."
+pip freeze > frozen-requirements.txt
+echo "Freezing...done"
